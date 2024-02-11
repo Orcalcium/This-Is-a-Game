4b612de09f9cd28c87fbf832c7de8e43428b54ad
jack254123 <99731903+jack254123@users.noreply.github.com>
2024-02-11 21:54:57 +0800

    deal with start button
    attach a listener to button's onClick() method and wrote a function to load the TestScene in the listener

--- Assets/Scripts/SceneManagement/MainMenu.cs
+++ Assets/Scripts/SceneManagement/MainMenu.cs
@@ -2,13 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 [DefaultExecutionOrder(1000)]
 public class MainMenu : MonoBehaviour
 {
+    public Button startButton;
     // Start is called before the first frame update
     void Start()
     {
-        SceneManager.LoadScene("Menu");
+        startButton.onClick.AddListener(StartNew);
+    }
+    void StartNew()
+    {
+        SceneManager.LoadScene("TestScene");
+        Debug.Log("Click");
     }
 
     // Update is called once per frame
